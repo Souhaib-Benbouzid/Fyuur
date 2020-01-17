2a6cd280648ad97f5d135bbd7ae844ee1b56c04e
Souhaib-Benbouzid <sohaib.code@gmail.com>
2020-01-17 08:48:53 +0100

    start point

--- app.py
+++ app.py
@@ -510,13 +510,12 @@ if not app.debug:
 # Launch.
 #----------------------------------------------------------------------------#
 
-# Default port:
+""" # Default port:
 if __name__ == '__main__':
     app.run()
 
 # Or specify port manually:
-'''
+ """
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+    app.run(host='0.0.0.0', debug=True)
+
